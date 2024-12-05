bf91f46c64ca6bc3106f4b583e100c6a81ce3c58
oguzhan18 <oguzhancart1@gmail.com>
2024-12-06 00:23:48 +0300

    vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Dolap Scraper API')
-    .setDescription('Dolap ürünlerini scrape eden API')
-    .setVersion('1.0')
-    .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup("api", app, document, {
+  .setTitle('Dolap Scraper API')
+  .setDescription('Dolap ürünlerini scrape eden API')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document, {
     customSiteTitle: "Dolap Scraper API",
     customfavIcon: "https://raw.githubusercontent.com/oguzhan18/seo-tools-api/main/assests/seo-tools-api-logo.png",
     customJs: [
@@ -26,7 +26,9 @@ async function bootstrap() {
       "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css"
     ]
   });
+
   app.useStaticAssets(join(__dirname, "..", "uploads"));
+
   await app.listen(3000);
 }
 
