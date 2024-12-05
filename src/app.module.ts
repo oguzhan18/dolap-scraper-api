a48807af3d318fc14b5036b9b0d6f75879c91095
oguzhan18 <oguzhancart1@gmail.com>
2024-12-06 00:26:34 +0300

    module remove

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ScraperModule } from './scraper/scraper.module';
 
 @Module({
-  imports: [ScraperModule],
-  controllers: [],
-  providers: [],
+  imports: [ScraperModule]
 })
 export class AppModule {}
